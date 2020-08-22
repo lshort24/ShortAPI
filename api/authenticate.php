300f391a9677053a560ef772d2afa36c3e2fd53e
Lisa <lshort24@gmailcom>
2020-08-22 16:13:22 -0600

    Match user to database user

--- api/authenticate.php
+++ api/authenticate.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require_once '../api/config/secrets.php';
+require_once './config/Database.php';
+require_once './config/secrets.php';
 
 $log = new Monolog\Logger('api');
 $log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/../app.log', Monolog\Logger::DEBUG));
@@ -10,30 +11,69 @@ $secrets = getSecrets();
 $origin = ($_SERVER['REMOTE_ADDR'] === $secrets['my_ip']) ? "http://localhost:3000" : 'https://shortsrecipes.com';
 header("Access-Control-Allow-Origin: {$origin}");
 header("Access-Control-Allow-Methods: POST");
-//header("Access-Control-Max-Age: 3600");
-//header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-//header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
-$authenticated = false;
-
-// get posted data
+// get posted data and validate the ID token
+$userId = null;
+$failReason = null;
 $data = json_decode(file_get_contents("php://input"));
 if (!empty($data->idToken)) {
     $client = new Google_Client(['client_id' => $secrets['googleClientId']]);
     $payload = $client->verifyIdToken($data->idToken);
     if ($payload) {
-        $userId = $payload['sub'];
-        $log->debug("ID token has been validated.");
-        $authenticated = true;
+        $userId = $payload['sub'];;
     }
     else {
-        $log->debug("Invalid ID Token");
+        $failReason = "Invalid ID Token";
     }
 }
 else {
-    $log->debug("No ID token was specified.");
+    $failReason = "No ID token was specified.";
 }
 
-$httpStatus = $authenticated ? 200 : 403;
-http_response_code($httpStatus);
+// Look the user up in our database
+$authenticated = false;
+if ($userId) {
+    $query = <<< MySQL
+            SELECT *
+            FROM users
+            WHERE 
+                user_id = :userId AND 
+                user_type = 'google'
+MySQL;
+
+    $params = [
+        ':userId' => [
+            "value" => $userId,
+            "type" => PDO::PARAM_STR
+        ]
+    ];
+
+    try {
+        $database = new Database();
+        $conn = $database->getConnection();
+        $stmt = $conn->prepare($query);
+
+        foreach ($params as $name => $param) {
+            $stmt->bindParam($name, $param["value"], $param["type"]);
+        }
+
+        $stmt->execute();
+        if ($stmt->rowCount() > 0) {
+            $authenticated = true;
+        }
+        else {
+            $failReason = "Could not find user in the database.";
+        }
+    }
+    catch (Exception $ex) {
+        $log->debug($ex->getMessage());
+    }
+}
+
+http_response_code(200);
+echo json_encode([
+    "authenticated" => $authenticated,
+    "failReason" => $failReason
+]);
