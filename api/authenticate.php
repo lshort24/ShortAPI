736f0cab3a8ffa960fbbb98f8990716b4622db97
Lisa <lshort24@gmailcom>
2020-12-05 21:06:04 -0700

    Refactored the authenticate endpoint

--- api/authenticate.php
+++ api/authenticate.php
@@ -1,11 +1,15 @@
 <?php
+session_start();
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
 require __DIR__ . '/../vendor/autoload.php';
 require_once './config/Database.php';
 require_once './config/secrets.php';
-session_start();
 
-$log = new Monolog\Logger('api');
-$log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/../app.log', Monolog\Logger::DEBUG));
+//date_default_timezone_set('America/Boise');
+$log = new Logger('api');
+$log->pushHandler(new StreamHandler(__DIR__ . '/../app.log', Logger::DEBUG));
 
 // required headers
 $secrets = getSecrets();
@@ -16,34 +20,30 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
-// get posted data and validate the ID token
-$userId = null;
-$failReason = null;
-$data = json_decode(file_get_contents("php://input"));
-if (!empty($data->idToken)) {
-    $client = new Google_Client(['client_id' => $secrets['googleClientId']]);
-    $payload = $client->verifyIdToken($data->idToken);
-    if ($payload) {
-        $userId = $payload['sub'];
-    }
-    else {
-        $failReason = "Invalid ID Token";
-    }
-}
-else {
-    $failReason = "No ID token was specified.";
-}
+function authenticate(string $googleClientId) : array {
+    // get posted data and validate the ID token
+    $data = json_decode(file_get_contents("php://input"));
 
-// Look the user up in our database
-$authenticated = false;
-if ($userId) {
-    $query = <<< MySQL
-            SELECT *
-            FROM users
-            WHERE 
-                user_id = :userId AND 
-                user_type = 'google'
-MySQL;
+    if (empty($data->idToken)) {
+        return [false, 'No ID token was specified.'];
+    }
+
+    $client = new Google_Client(['client_id' => $googleClientId]);
+    $payload = $client->verifyIdToken($data->idToken);
+    if (empty($payload)) {
+        return [false, 'Invalid ID Token.'];
+    }
+
+    $userId = $payload['sub'];
+
+    // Look the user up in our database
+    $query = "
+        SELECT *
+        FROM users
+        WHERE 
+            user_id = :userId AND 
+            user_type = 'google'
+    ";
 
     $params = [
         ':userId' => [
@@ -52,32 +52,34 @@ MySQL;
         ]
     ];
 
-    try {
-        $database = new Database();
-        $conn = $database->getConnection();
-        $stmt = $conn->prepare($query);
+    $database = new Database();
+    $conn = $database->getConnection();
+    $stmt = $conn->prepare($query);
 
-        foreach ($params as $name => $param) {
-            $stmt->bindParam($name, $param["value"], $param["type"]);
-        }
+    foreach ($params as $name => $param) {
+        $stmt->bindParam($name, $param["value"], $param["type"]);
+    }
 
-        $stmt->execute();
-        if ($stmt->rowCount() > 0) {
-            $authenticated = true;
-        }
-        else {
-            $failReason = "Could not find user in the database.";
-        }
+    $stmt->execute();
+    if ($stmt->rowCount() === 0) {
+        return [false, 'Could not find user in the database.'];
     }
-    catch (Exception $ex) {
-        $log->debug($ex->getMessage());
-    }
+    return [true, ''];
 }
 
-$_SESSION['idToken'] = $authenticated ? $data->idToken : '';
-
-http_response_code(200);
-echo json_encode([
-    "authenticated" => $authenticated,
-    "failReason" => $failReason
-]);
+try {
+    [$authenticated, $failReason] = authenticate($secrets['googleClientId']);
+    http_response_code(200);
+    echo json_encode([
+        'authenticated' => $authenticated,
+        'failReason' => $failReason
+    ]);
+}
+catch (Exception $ex) {
+    $log->debug($ex->getMessage());
+    http_response_code(500);
+    echo json_encode([
+        'authenticated' => false,
+        'failReason' => $ex->getMessage()
+    ]);
+}
