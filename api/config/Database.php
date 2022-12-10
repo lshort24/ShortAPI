cc87b9634ca52a5209227eff639a50d00922cf85
Lisa <lshort24@gmailcom>
2022-12-10 11:28:56 -0700

    Refactored Database class to prepare for multiple users roles

--- api/config/Database.php
+++ api/config/Database.php
@@ -7,31 +7,26 @@ use PDO;
 require_once 'secrets.php';
 
 class Database {
-    // specify your own database credentials
-    private $host = "localhost";
-    //private $db_name = "timeline";
-    private $username = "webuser";
-    public $conn;
-
     /**
      * Get the database connection
      *
      * @param string $db_name
      * @return PDO
      */
-    public function getConnection(string $db_name): PDO
+    public function getConnection(string $db_name) : PDO
     {
-        $this->conn = null;
         $secrets = getSecrets();
+        $host = $secrets['db_host'];
+        $username = $secrets['username']['user'];
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $db_name, $this->username, $secrets['db_password']);
-            $this->conn->exec("set names utf8");
+            $conn = new PDO("mysql:host=" . $host . ";dbname=" . $db_name, $username, $secrets['db_password']);
+            $conn->exec("set names utf8");
         }
         catch(Exception $exception){
             die("Connection error: " . $exception->getMessage());
         }
 
-        return $this->conn;
+        return $conn;
     }
 }
